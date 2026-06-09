195fb0e6c186d7d30b22eed380ae53cee3ba507d
Axel H. Karlsson <git@axelkarlsson.se>
2026-06-09 23:24:37 +0200

    resolve #5

--- app/src/main/java/se/axelkarlsson/hydroxide/ui/component/AppItem.kt
+++ app/src/main/java/se/axelkarlsson/hydroxide/ui/component/AppItem.kt
@@ -3,20 +3,25 @@ package se.axelkarlsson.hydroxide.ui.component
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.core.graphics.drawable.toBitmap
 import se.axelkarlsson.hydroxide.app.App
 
 @Composable
-fun AppItem(app: App, onClick: () -> Unit = {}, onLongClick: () -> Unit = {}) {
+fun AppItem(app: App, onClick: () -> Unit = {}, onLongClick: () -> Unit = {}, size: Dp = 72.dp) {
     Column(
         modifier = Modifier
             .padding(8.dp)
             .combinedClickable(onClick = onClick, onLongClick = onLongClick)
+            .size(size)
     ) {
         Image(
             bitmap = app.metadata.icon.rounded?.asImageBitmap()
@@ -24,7 +29,10 @@ fun AppItem(app: App, onClick: () -> Unit = {}, onLongClick: () -> Unit = {}) {
                     app.metadata.icon.drawable.intrinsicWidth,
                     app.metadata.icon.drawable.intrinsicHeight
                 ).asImageBitmap(),
-            contentDescription = null
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .fillMaxSize()
         )
     }
 }
